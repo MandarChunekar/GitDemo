7057280bfac45c5f7bafd91cc04bd2ab1a5b503c
MandarChunekar <cmandar29@gmail.com>
2023-12-28 13:20:12 +0530

    this is commit from mandar

--- src/main/java/Mandar/App.java
+++ src/main/java/Mandar/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello mandar!" );
+        System.out.println( "welcome!" );
+        System.out.println( "to" );
+        System.out.println( "the" );
+        System.out.println( "World" );
+        System.out.println( "of!" );
+        System.out.println("GitHub" );
+        
     }
 }
